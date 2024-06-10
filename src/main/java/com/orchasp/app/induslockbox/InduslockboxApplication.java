a7e30f76c0dd496ec11ef5424be72bb4ca7ad722
RameshOrchasp <t-krameshbabu@orchasp.com>
2024-06-10 16:39:28 +0530

    Initial commit

---
+++ src/main/java/com/orchasp/app/induslockbox/InduslockboxApplication.java
@@ -0,0 +1,13 @@
+package com.orchasp.app.induslockbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InduslockboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InduslockboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orchasp/app/induslockbox/InduslockboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchasp.app.induslockbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InduslockboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
