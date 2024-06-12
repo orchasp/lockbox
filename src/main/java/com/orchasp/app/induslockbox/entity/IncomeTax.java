5327095a77b664043a6d53e67fd4b6a0b8796073
ShruthiOrchasp <t-gshruthi@orchasp.com>
2024-06-12 11:26:22 +0530

    sruthi

--- src/main/java/com/orchasp/app/induslockbox/entity/IncomeTax.java
+++ src/main/java/com/orchasp/app/induslockbox/entity/IncomeTax.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class IncomeTax {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+      @Size(max=12)
 	private String panNumber;
 	
 	private String name;
